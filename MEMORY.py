2f2404a1ccf7ebd9040ba7dfe80f3f25c6ca90b1
Wackee <WackeeeEEE@gmail.com>
2021-08-10 11:23:22 -0700

    Initial Commit

---
+++ MEMORY.py
@@ -0,0 +1,312 @@
+from ctypes import *
+from ctypes.wintypes import *
+import psutil
+
+from struct import *
+
+import json
+from types import SimpleNamespace
+
+import asyncio
+#import threading
+import time
+
+k32 = windll.kernel32
+openProc = k32.OpenProcess
+openProc.argtypes = DWORD, BOOL, DWORD
+openProc.restype = HANDLE
+readProcMem = k32.ReadProcessMemory
+readProcMem.argtypes = [HANDLE, LPCVOID, LPVOID, c_size_t, POINTER(c_size_t)]
+readProcMem.restype = BOOL
+writeProcMem = k32.WriteProcessMemory
+writeProcMem.argtypes = [HANDLE, LPVOID, LPCVOID, c_size_t, POINTER(c_size_t)]
+psapi = windll.psapi
+psapi.GetModuleFileNameExA.argtypes = [HANDLE, HMODULE, LPSTR, DWORD]
+psapi.GetModuleFileNameExA.restype = BOOL
+
+
+nameProcess = "MCC-Win64-Shipping.exe"
+playerOffsets = json.load(open("playerOffsets.json", "r"), object_hook=lambda d: SimpleNamespace(**d)) # might wanna change this, namespace stuff scary
+strToType = {
+    "string":str(),
+    "int":int(),
+    "float":float()
+}
+
+PROCESS_VM_READ = 0x0010 # READ-ONLY
+PROCESS_ALL_ACCESS = 0x1F0FFF # MORE_ACCESS
+PROCESS_QUERY_INFORMATION = 0x0400
+
+# Fun starts here
+
+class ModuleInformation:
+    def __init__(self, index, name, path, address):
+        self.index = index
+        self.name = name
+        self.path = path
+        self.address = address
+
+class Process:
+    def __init__(self, pid, name):
+        self.pid = pid
+        self.handle = openProc(PROCESS_ALL_ACCESS, False, self.pid)
+        self.mods = getModules(self.handle)
+        self.name = name
+        self.address = None
+        for mod in self.mods:
+            if mod.name == self.name:
+                self.address = mod.address
+        if self.address == None:
+            print("PROCESS ADDRESS NOT FOUND")
+        else:
+            print(f"Process {self.name} found at {self.address}")
+
+    def readP(self, address, size):#, datatype=None): # size bytes
+        buffer = create_string_buffer(size)
+        counter = c_ulonglong()
+        a = readProcMem(self.handle, address, buffer, size, byref(counter))
+        if a == 0:
+            print(f"ERROR: {GetLastError()}\n{FormatError(GetLastError())}")
+        else:
+            return Fragment(address, buffer.raw) #data
+
+    def readDeepP(self, deepPointer, size, datatype=None):
+        pt = self.readP(self.address+deepPointer[0], 8).asPtr()
+        for offset in deepPointer[1:-1]:
+            pt = self.readP(pt+offset, 8).asPtr()
+        data = self.readP(pt+deepPointer[-1], size)
+        if datatype != None: # might implement this on the next highest level, not sure yet - ex: data return will be as"Type"()
+            if type(datatype) == type(int()):
+                #print(f"as type {type(datatype)}: {data.asInt()}")
+                if len(data.raw) == 4:
+                    return data.asInt32()
+            if type(datatype) == type(str()):
+                #print(f"as type {type(datatype)}: {data.asStr()}")
+                return data.asStr()
+            if type(datatype) == type(float()):
+                #print(f"as type {type(datatype)}: {data.asFloat()}")
+                return data.asFloat()
+            # if type(datatype) == type(ptr()): #eeeeeeeh, do i have to?
+            #     print("as type {type(datatype)}: {data.asPtr()}")
+        return data
+
+    def listModules(self, key=None):
+        for mod in self.mods:
+            if key == None:
+                print(f"Index: {mod.index} | Name: {mod.name} | Address: {mod.address}")
+            else:
+                if mod.name == key:
+                    print(f"Index: {mod.index} | Name: {mod.name} | Address: {mod.address}")
+
+
+    ### Testy Stuff ###
+
+    def printLevel(self):
+        level = playerOffsets.halo1.level
+        #levelPointer = Pointer(level.offsets, level.length, level.type)# shorten
+        levelPointer = PointerShort(level)
+        levelFragment = self.readDeepP(levelPointer.offsets, levelPointer.length, levelPointer.type)
+        # print(f"Current level: {levelFragment.asStr()}")
+
+class Fragment:
+    def __init__(self, address, raw):
+        self.address = address
+        self.raw = raw
+        self.size = len(self.raw)
+        
+    def __doc__():
+        return "Arbitrarily sized block of process data"
+    def asInt(self):
+        return unpack("<Q", self.raw)[0]
+    def asInt32(self):
+        return unpack("<i", self.raw)[0]
+    def asStr(self):
+        return self.raw.decode('utf-8')
+    def asFloat(self):
+        return unpack("<f", self.raw)[0]
+    def asDouble(self):
+        return unpack("<d", self.raw)[0]
+    def asPtr(self): #kinda just for clarity, dont think i really need
+        return unpack("<Q", self.raw)[0]
+
+class Pointer:
+    def __init__(self, offsets, length, type): # list/tuple in
+
+        self.offsets = [] # converting string hex/int? to int
+        for offset in offsets:
+            self.offsets.append(int(offset, 0)) # using 0 base to invoke guessing base, useful for different types in db?
+
+        self.length = length
+        self.type = strToType[type]
+
+class PointerShort:
+    def __init__(self, pointerObj): # composite arg in
+
+        self.offsets = [] # converting string hex/int? to int
+        for offset in pointerObj.offsets:
+            self.offsets.append(int(offset, 0)) # using 0 base to invoke guessing base, useful for different types in db?
+
+        self.length = pointerObj.length
+        self.type = strToType[pointerObj.type]
+
+class Governor:
+    def __init__(self):
+        self.objects = []
+        self.timeframes = []
+        self.fpsWatcher = WatcherFPS()
+        
+
+    def ready(self, obj):
+        if time.time() >= obj.lastRun+obj.interval:
+            obj.run()
+            self.timeframes.append(time.time())
+
+    def addWatcher(self, obj):
+        self.objects.append(obj)
+
+    def listWatchers(self):
+        watcherStr = str('\n'.join([obj.name for obj in self.objects]))
+        print(f"Current Watchers:{watcherStr}")
+
+    def fps(self):
+        counter = 0
+        if time.time() > self.fpsWatcher.lastRun + self.fpsWatcher.interval:
+            self.fpsWatcher.lastRun = time.time()
+            if len(self.timeframes) > 500:
+                for timeframe in reversed(self.timeframes):
+                    if time.time() < timeframe + 1:
+                        counter += 1
+                for object in self.objects:
+                    print(object.val)
+                print(f"Reading game memory at {counter} frames per second")
+
+    def objectData(self, watcherName):
+        return filter(lambda x:x.name == watcherName, self.objects)[0].val
+
+    def loop(self):
+        while True:
+            #time.sleep(5)
+            for obj in self.objects:
+                self.ready(obj)
+            self.fps()
+
+
+class Watcher:
+    def __init__(self, proc, pointer, name, interval=1):
+        self.proc = proc
+        self.pointer = pointer
+        self.name = name
+        self.interval = interval
+        self.lastRun = time.time()
+        self.run()
+
+    def run(self):
+        self.val = self.proc.readDeepP(self.pointer.offsets, self.pointer.length, datatype=self.pointer.type)
+        #print(self.val)
+        self.lastRun = time.time()
+   
+
+class WatcherFPS:
+    def __init__(self):
+        self.interval = 5
+        self.lastRun = time.time()
+
+class Phone:
+    def __init__(self, string):
+        self.string = string
+    
+    def __repr__(self) -> str:
+        return self.string
+
+def getPIDs(nameProcess):
+    found = []
+    for proc in psutil.process_iter():
+        if str(nameProcess) in str(proc.name):
+            print("FOUND", nameProcess)
+            found.append(proc.pid)
+    try:
+        if len(found) > 0:
+            return found
+    except Exception as e:
+        print(type(e), e)
+        return None
+
+def getProc(pid):
+
+    # Get a handle to the process
+    proc = openProc( (PROCESS_VM_READ | PROCESS_QUERY_INFORMATION) , False, pid)
+    if proc == 0:
+        print(f"ERROR: {GetLastError()}\n{FormatError(GetLastError())}")
+    else:
+        return proc
+
+def getModules(proc):
+
+    # Get a handle to the process
+    hProcess = proc
+
+    # Set up vars
+    arr = HMODULE * 1024
+    hMods = arr() 
+    cbNeeded = c_ulong()
+    modNameArray = c_char * MAX_PATH
+    clearName = modNameArray()
+    szModName = modNameArray()
+
+    # Get a list of all the modules in this process
+    EnumAttempt = psapi.EnumProcessModules(hProcess, hMods, sizeof(hMods), byref(cbNeeded))
+    if EnumAttempt == 0:
+        print(GetLastError())
+    else:
+        mods = []
+
+        for i, val in enumerate(hMods[:int(cbNeeded.value / sizeof(HMODULE))]):
+            memmove(byref(szModName), byref(clearName), sizeof(clearName))
+            if val != None:
+                modAttempt = psapi.GetModuleFileNameExA(hProcess, val, szModName, int(sizeof(szModName) / sizeof(c_char)))
+                if modAttempt == 0:
+                    print(GetLastError())
+                else:
+                    index = i
+                    name = str(b''.join([i for i in szModName if i != b'\x00'])).split("\\")[-1][:-1]
+                    path = str(b''.join([i for i in szModName if i != b'\x00']))[2:-1]
+                    address = val
+                    mods.append(ModuleInformation(index, name, path, address))
+            else:
+                pass
+        return mods
+
+def printModules(pid, proc):
+
+    print(f"Modules of process {pid}:")
+    proc = getProc(pid)
+    mods = getModules(proc)
+
+    for mod in mods:
+        print(f"Index: {mod.index}\nModule: {mod.name}\nVirtual Address: {mod.address}")
+
+def start_here():
+    nameProcess = "MCC-Win64-Shipping.exe"
+    PIDs = getPIDs(nameProcess)
+    if type(PIDs) == type(None):
+        print(f"Process {nameProcess} not found")
+        proc = None
+    else:
+        print(f"{len(PIDs)} Processes found, acting on first PID - {PIDs[0]}")
+        print(PIDs)
+        proc = Process(PIDs[0], nameProcess)
+    return proc
+
+    
+MCC = start_here()
+g = Governor()
+# level = playerOffsets.halo1.level
+# levelWatcher = Watcher(MCC, PointerShort(level), name="h1-level", interval=5)
+
+h3xpos = playerOffsets.halo3.xpos
+h3xposWatcher = Watcher(MCC, PointerShort(h3xpos), name="h3xpos", interval=.005)
+#g.addWatcher(h3xposWatcher)
+h3ypos = playerOffsets.halo3.ypos
+h3yposWatcher = Watcher(MCC, PointerShort(h3ypos), name="h3ypos", interval=.005)
+#g.addWatcher(h3yposWatcher)
+#g.loop()
---
+++ YEP DOTS.py
@@ -0,0 +1,193 @@
+import cv2
+import numpy as np
+import json
+import os
+import MEMORY
+import asyncio
+
+imgName = "centerOfClip_marked.PNG"
+img = cv2.imread(imgName)
+
+# Load Offset Config
+imgOffsetFile = open("imgOffsets.json", "r+")
+imgOffsetData = json.load(imgOffsetFile)
+imgOffsetFile.close()
+
+playerOffsetFile = open("playerOffsets.json", "r+")
+playerOffsetData = json.load(playerOffsetFile)
+playerOffsetFile.close()
+
+
+# Create ImageOffsets class
+class ImageOffsets:
+    def __init__(self):
+        self.name = imgName
+        self.scale = imgOffsetData[imgName]["scale"]
+        self.start_x = imgOffsetData[imgName]["start_x"]
+        self.center_x = imgOffsetData[imgName]["center_x"]
+        self.end_x = imgOffsetData[imgName]["end_x"]
+        self.start_y = imgOffsetData[imgName]["start_y"]
+        self.center_y = imgOffsetData[imgName]["center_y"]
+        self.end_y = imgOffsetData[imgName]["end_y"]
+    def update(self):
+        self.__init__()
+
+# Define Starting Clip Position (for scaling coordinates to image)
+CLIP_START_X = -22.715
+CLIP_START_Y = 29.927
+
+CLIP_END_X = -30.685
+CLIP_END_Y = 3.3
+
+# Define Window Parameters
+
+# Intended window resolution/scale (not needed anymore, using dynamic res calc)
+WIN_RES_X = 241
+WIN_RES_Y = 726
+
+# Image Margins (extra pixels beyond start & end)
+IMG_MARGIN_X = 20
+IMG_MARGIN_Y = 20
+
+# Window Padding to make sure everything is presented (not needed anymore, i'm dumb)
+WIN_PAD_X = 89
+WIN_PAD_Y = 200
+
+# Instantiate imgOffsets for current reference image
+imgOffsets = ImageOffsets()
+
+print(f"name: {imgOffsets.name}\nscale: {imgOffsets.scale}\nx: {imgOffsets.start_x}\ny: {imgOffsets.start_y}")
+
+# Define shifting image to match window
+def shift_img(img, imgOffsets):
+    img_rows, img_cols = img.shape[:2]
+    #translation_matrix = np.float32([ [1, 0, (-1 * imgOffsets.start_x - IMG_MARGIN_X - WIN_PAD_X) ], [0, 1, (-1 * imgOffsets.start_y - IMG_MARGIN_Y - WIN_PAD_Y) ] ])
+    translation_matrix = np.float32([ [1, 0, (-1 * imgOffsets.end_x) + IMG_MARGIN_X ], [0, 1, (-1 * imgOffsets.start_y) + IMG_MARGIN_Y ] ])
+    shiftedImg = cv2.warpAffine(img, translation_matrix, (img_cols, img_rows))
+    return shiftedImg
+
+# Logic to convert between pixels and ingame coords
+def coordsToPixels(a, b, scale):
+    x = int( ( ( IMG_MARGIN_X * scale ) - (CLIP_END_X - a ) ) / scale )
+    y = int( ( ( IMG_MARGIN_Y * scale ) + (CLIP_START_Y - b ) ) / scale )
+    print(f"IGC: {playerX}, {playerY}\nPx: {x}, {y}")
+    return (x,y)
+
+# Create cv2 window
+windowName = f"Silo Clip Viewer - {imgOffsets.name}"
+cv2.namedWindow(windowName, cv2.WINDOW_AUTOSIZE)
+img = cv2.imread(imgName)
+shiftedImg = shift_img(img, imgOffsets)
+
+imgScale = (CLIP_START_X - CLIP_END_X) / (imgOffsets.start_x - imgOffsets.end_x)
+print(f"Scale: {imgScale}")
+#leftMarginPx = 20 * imgScale
+playerX = MEMORY.h3xposWatcher.val
+playerY = MEMORY.h3yposWatcher.val
+
+playerPx = coordsToPixels(playerX, playerY, imgScale)
+
+#playerDot = cv2.circle(shiftedImg, ((imgOffsets.start_x - imgOffsets.end_x) + IMG_MARGIN_X, int(IMG_MARGIN_Y / 2)), 2, (0, 255, 0), -1)
+playerDot = cv2.circle(shiftedImg, playerPx, 2, (0, 255, 0), -1)
+cv2.imshow(windowName, playerDot)
+cv2.resizeWindow(windowName, (imgOffsets.start_x - imgOffsets.end_x) + 2 * IMG_MARGIN_X, (imgOffsets.end_y - imgOffsets.start_y) + 2 * IMG_MARGIN_Y) # adding margins to window resolution
+cv2.waitKey(0)
+
+# while True:
+#     # MEMORY.
+#     cv2.imshow(windowName, shiftedImg)
+#     if cv2.waitKey(16) & 0xFF == ord('1'):
+#         break
+cv2.destroyAllWindows()
+
+#241, 726
+#300, 7
+
+#cv2.imshow()
+#cv2.waitKey(16)
+
+# width = 100
+# height = 100
+
+# # Make empty black image of size (100,100)
+# img = np.zeros((height, width, 3), np.uint8)
+
+# red = [0,0,255]
+
+# # Change pixel (50,50) to red
+# img[50,50] = red
+
+# cv2.imshow('img', img)
+# cv2.waitKey(0)
+
+
+_reference = """
+-22.715 29.927 -4.638 / 70.28 -85.50 / 1.000
+-22.715 = 660px - x
+29.927 = 85px - y
+-30.685 = 450px - endx
+3.373 = 765px - endy
+
+
+
+autoaimed to / 71.63  
+
+-30.685 3.373 -6.280 / 1.000
+
+-26.7 16.65 -5.459
+
+<CheatEntry>
+      <ID>25948</ID>
+      <Description>"h3 x pos"</Description>
+      <VariableType>Float</VariableType>
+      <Address>"halo3.dll"+01164BEC</Address>
+      <Offsets>
+        <Offset>4</Offset>
+      </Offsets>
+    </CheatEntry>
+    <CheatEntry>
+      <ID>25949</ID>
+      <Description>"h3 y pos"</Description>
+      <VariableType>Float</VariableType>
+      <Address>"halo3.dll"+01164BEC</Address>
+      <Offsets>
+        <Offset>8</Offset>
+      </Offsets>
+    </CheatEntry>
+    <CheatEntry>
+      <ID>25950</ID>
+      <Description>"h3 z pos"</Description>
+      <VariableType>Float</VariableType>
+      <Address>"halo3.dll"+01164BEC</Address>
+      <Offsets>
+        <Offset>C</Offset>
+      </Offsets>
+    </CheatEntry>
+    <CheatEntry>
+      <ID>25945</ID>
+      <Description>"h3 x vel"</Description>
+      <VariableType>Float</VariableType>
+      <Address>"halo3.dll"+01164BEC</Address>
+      <Offsets>
+        <Offset>44</Offset>
+      </Offsets>
+    </CheatEntry>
+    <CheatEntry>
+      <ID>25946</ID>
+      <Description>"h3 y vel"</Description>
+      <VariableType>Float</VariableType>
+      <Address>"halo3.dll"+01164BEC</Address>
+      <Offsets>
+        <Offset>48</Offset>
+      </Offsets>
+    </CheatEntry>
+    <CheatEntry>
+      <ID>25947</ID>
+      <Description>"h3 z vel"</Description>
+      <VariableType>Float</VariableType>
+      <Address>"halo3.dll"+01164BEC</Address>
+      <Offsets>
+        <Offset>4C</Offset>
+      </Offsets>
+    </CheatEntry>
+    """
